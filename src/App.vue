e41d6d00465e0aae5cc4d6207dc980238a7e836d
Austin Thornley <austinjt1012@gmail.com>
2021-11-14 22:13:21 -0700

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<h1>{{ title }}</h1>
+	<input type="text" id="inputField" class="inputField"/>
+	<button @click="addToDoItem" id="addToDo" class="addToDo">Add Item</button>
+	<ul class="results" id="results">
+
+	</ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+	data() {
+		return {
+			title: 'To-Do App',
+		}
+	},
+	methods: {
+		addToDoItem() {
+			let input = document.getElementById('inputField');
+			let newElement = document.createElement('li');
+			let results = document.getElementById('results');
+
+			if(input.value) {
+				newElement.innerText = input.value;
+				results.appendChild(newElement);
+				input.value = '';
+
+				newElement.addEventListener('click', function() {
+					results.removeChild(newElement);
+				})
+			} else {
+				alert('Please enter some text');
+			}
+		}
+	}
 }
 </script>
 
@@ -23,4 +46,22 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.inputField {
+	font-size: 2rem;
+	margin-right: 1rem;
+}
+
+.addToDo {
+	height: 2rem;
+}
+
+.results li {
+	list-style-type: none;
+}
+
+.results li:hover{
+	color: red;
+}
+
 </style>
